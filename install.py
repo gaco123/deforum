8e63674397525f30190268d6b85812eba46e636f
gaco123 <104175926+gaco123@users.noreply.github.com>
2023-09-14 18:40:28 -0500

    Update install.py

--- install.py
+++ install.py
@@ -1,4 +1,4 @@
-import launch
+import sus
 import os
 
 req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
@@ -6,5 +6,5 @@ req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requiremen
 with open(req_file) as file:
     for lib in file:
         lib = lib.strip()
-        if not launch.is_installed(lib):
-            launch.run_pip(f"install {lib}", f"Deforum requirement: {lib}")
+        if not sus.is_installed(lib):
+            sus.run_pip(f"install {lib}", f"Deforum requirement: {lib}")
